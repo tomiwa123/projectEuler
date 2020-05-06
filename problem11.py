59aafe6f0edfab6e14b75a1f244b57f6490bc57f
Ayotomiwa Akinyele <ayotomiwa.akinyele@gmail.com>
2020-05-06 10:49:02 -0400

    10 code looks correct but not working

--- problem11.py
+++ problem11.py
@@ -1,4 +1,5 @@
 if __name__ == '__main__':
+    # Dont work for some reason
     grid = [
         "08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08",
         "49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00",
@@ -39,5 +40,9 @@ if __name__ == '__main__':
             if col <= len(grid[0]) - 4:
                 potential_max = grid[row][col] * grid[row][col + 1] * \
                                 grid[row][col + 2] * grid[row][col + 3]
+                max_found = max(max_found, potential_max)
+            if row >= 3 and col >= 3:
+                potential_max = grid[row][col] * grid[row - 1][col - 1] * \
+                                grid[row - 2][col - 2] * grid[row - 3][col - 3]
                 max_found = max(max_found, potential_max)
     print(max_found)
