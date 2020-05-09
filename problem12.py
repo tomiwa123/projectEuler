1462bf66be6a99b0ab08f1ce69c11241eeeb3bfd
Ayotomiwa Akinyele <ayotomiwa.akinyele@gmail.com>
2020-05-09 00:43:31 -0400

    Solved 12

---
+++ problem12.py
@@ -0,0 +1,30 @@
+if __name__ == '__main__':
+
+    import math
+
+
+    def count_factors(num):
+        count = 0
+        perfect_square = False
+        for divisor in range(1, math.floor(num ** 0.5) + 1):
+            if num % divisor == 0:
+                count += 1
+            if divisor == num ** 0.5:
+                perfect_square = True
+
+        if perfect_square:
+            return (count * 2) - 1
+        return count * 2
+
+
+    tally = 0
+
+    answer = 0
+    current = 1
+    while answer == 0:
+        tally += current
+        if count_factors(tally) > 500:
+            answer = tally
+        current += 1
+
+    print(tally)
