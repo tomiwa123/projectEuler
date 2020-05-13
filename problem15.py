dbe6aa0b628809b535f9b7e5e265e3d9503b02c2
Tomiwa Akinyele <ayotomiwa.akinyele@gmail.com>
2020-05-13 02:50:38 -0400

    Solved Problem 15

---
+++ problem15.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    total_product = 1
+    for num in range(21, 41):
+        total_product *= num
+    for num in range(1, 21):
+        total_product /= num
+
+    print(total_product)
