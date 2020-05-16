c734ceb9084291190ff1441a84b5bc5794c30024
Tomiwa Akinyele <ayotomiwa.akinyele@gmail.com>
2020-05-16 02:57:19 -0400

    Solved Problem 21

--- problem21.py
+++ problem21.py
@@ -1,2 +1,23 @@
 if __name__ == '__main__':
-    
+    def sum_divisors(number):
+        divisor_sum = 0
+        for poss_factor in range(1, int(number ** 0.5) + 1):
+            if number % poss_factor == 0:
+                divisor_sum += poss_factor
+                if poss_factor not in {number / poss_factor, 1}:
+                    divisor_sum += number // poss_factor
+        return divisor_sum
+    print(sum_divisors(220))
+
+
+    amicable_sum = 0
+    store = ["NOT TOUCH"]
+    for num in range(1, 10001):
+        store.append(sum_divisors(num))
+    for num in range(1, 10001):
+        potential_pair = store[num]
+        if 1 <= potential_pair <= 10000:
+            if num == store[potential_pair] and num != potential_pair:
+                amicable_sum += num
+
+    print(amicable_sum)
