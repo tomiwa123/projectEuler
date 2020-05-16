d1ce8d59b52795e37c7e69c23a8e72a3c7c9c800
Tomiwa Akinyele <ayotomiwa.akinyele@gmail.com>
2020-05-16 02:22:58 -0400

    Solved Problem 20

--- problem20.py
+++ problem20.py
@@ -1,2 +1,8 @@
 if __name__ == '__main__':
-    
+    product = 1
+    for num in range(2, 101):
+        product *= num
+    sum = 0
+    for let in str(product):
+        sum += int(let)
+    print(sum)
