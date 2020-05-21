f0a5458563cf1cb1fb020f1b22af20b22e67bd3d
Tomiwa Akinyele <ayotomiwa.akinyele@gmail.com>
2020-05-21 16:18:10 -0400

    Solved Problem 29

--- problem29.py
+++ problem29.py
@@ -1,2 +1,6 @@
 if __name__ == '__main__':
-    
+    set_of_powers = set([])
+    for a in range(2, 101):
+        for b in range(2, 101):
+            set_of_powers.add(a ** b)
+    print(len(set_of_powers))
