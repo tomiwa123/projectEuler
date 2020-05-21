cd7a7419e845d97ac55a94fbd0d28cae19924856
Tomiwa Akinyele <ayotomiwa.akinyele@gmail.com>
2020-05-21 16:02:32 -0400

    Attempt 1 Problem 28

--- problem28.py
+++ problem28.py
@@ -1,2 +1,13 @@
 if __name__ == '__main__':
-    
+    current = 1
+    step = 2
+    numbers = [1]
+    while current <= 1001 * 1001:
+        for _ in range(4):
+            current += step
+            numbers.append(current)
+        step += 2
+    num_sum = 1
+    for num in range(1, len(numbers), 2):
+        num_sum += num
+    print(num_sum)
