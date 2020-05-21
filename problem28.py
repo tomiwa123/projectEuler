f254eea9188bac156bf8af29a80b71f2749cb8db
Tomiwa Akinyele <ayotomiwa.akinyele@gmail.com>
2020-05-21 16:11:48 -0400

    Solved Problem 28

--- problem28.py
+++ problem28.py
@@ -2,12 +2,13 @@ if __name__ == '__main__':
     current = 1
     step = 2
     numbers = [1]
-    while current <= 1001 * 1001:
+    while current <= 1000 * 1000:
         for _ in range(4):
             current += step
             numbers.append(current)
         step += 2
+        print(current ** 0.5)
     num_sum = 1
-    for num in range(1, len(numbers), 2):
-        num_sum += num
+    for num in range(1, len(numbers)):
+        num_sum += numbers[num]
     print(num_sum)
