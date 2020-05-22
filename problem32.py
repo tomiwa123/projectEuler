33c4057706dc1c00b0338631391593373502b092
Tomiwa Akinyele <ayotomiwa.akinyele@gmail.com>
2020-05-22 00:50:30 -0400

    Attempt 1 Problem 32

--- problem32.py
+++ problem32.py
@@ -1,2 +1,14 @@
 if __name__ == '__main__':
-    print("Hello"[::-1])
+
+    num_list = list(range(1, 10))
+    used_set = set([])
+    for a in num_list:
+        used_set.add(a)
+        list_a = list(filter(lambda num: num not in used_set, num_list))
+        for b in list_a:
+            used_set.add(b)
+            list_b = list(filter(lambda num: num not in used_set, num_list))
+            for c in list_b:
+                use
+            used_set.remove(b)
+        used_set.remove(a)
