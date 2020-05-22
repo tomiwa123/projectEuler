4b8f0d7e0814e9d175baf5eb3fdc50294ca86e61
Tomiwa Akinyele <ayotomiwa.akinyele@gmail.com>
2020-05-22 01:18:01 -0400

    Attempt 2 Problem 32

--- problem32.py
+++ problem32.py
@@ -1,14 +1,19 @@
 if __name__ == '__main__':
 
-    num_list = list(range(1, 10))
-    used_set = set([])
-    for a in num_list:
-        used_set.add(a)
-        list_a = list(filter(lambda num: num not in used_set, num_list))
-        for b in list_a:
-            used_set.add(b)
-            list_b = list(filter(lambda num: num not in used_set, num_list))
-            for c in list_b:
-                use
-            used_set.remove(b)
-        used_set.remove(a)
+    track = []
+    for a in range(1, 10000):
+        for b in range(1, 10000):
+            if a == b:
+                continue
+            string_a = str(a)
+            string_b = str(b)
+            string_c = str(a * b)
+            sum_of_strings = string_a + string_b + string_c
+            if len(sum_of_strings) == len(set(sum_of_strings)) == 9:
+                track.append(a * b)
+    count = 0
+    for num in set(track):
+        count += num
+    print(count)
+
+
