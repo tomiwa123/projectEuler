d505e560afcf9990708c6f1ac844bd598489b34b
Tomiwa Akinyele <ayotomiwa.akinyele@gmail.com>
2020-05-22 01:26:49 -0400

    Solved Problem 32

--- problem32.py
+++ problem32.py
@@ -3,14 +3,17 @@ if __name__ == '__main__':
     track = []
     for a in range(1, 10000):
         for b in range(1, 10000):
-            if a == b:
-                continue
+            if (a > 1000 and b > 1000) or b >= a:
+                break
             string_a = str(a)
             string_b = str(b)
             string_c = str(a * b)
             sum_of_strings = string_a + string_b + string_c
+            if "0" in sum_of_strings:
+                continue
             if len(sum_of_strings) == len(set(sum_of_strings)) == 9:
                 track.append(a * b)
+                print(a, b, a * b)
     count = 0
     for num in set(track):
         count += num
