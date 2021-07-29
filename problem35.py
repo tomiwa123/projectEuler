92b3941ef6b5badc4ea719527d4672cea67e903f
tomiwa123 <ayotomiwa.akinyele@gmail.com>
2021-07-29 13:56:01 -0700

    Problem 35

--- problem35.py
+++ problem35.py
@@ -1,3 +1,5 @@
+import itertools
+
 if __name__ == '__main__':
   def is_prime(num):
         if num % 2 == 0 or num <= 1:
@@ -6,15 +8,38 @@ if __name__ == '__main__':
             if num % x == 0:
                 return False
         return True
-    
+  
   def is_circular_prime(num):
     # circular prime cannot contain digits 2, 4, 6, 8, 5, 0
+    string_num = str(num)
     for test_num in ['0', '2', '4', '5', '6', '8']:
-      if test_num in str(num):
+      if test_num in string_num:
         return False
-    print(num)
     
+    if num in answers:
+      return False
+
+    list_of_permutations = list(itertools.permutations(list(string_num)))
+    length = len(string_num)
+    new_coprimes = []
+
+    for permutation in list_of_permutations:
+      number = 0
+      for index, digit in enumerate(permutation):
+        number += int(digit) * pow(10, length - index - 1)
+      if not is_prime(number):
+        return False
+      new_coprimes.append(number)
+    
+    return new_coprimes
     # number contains only 1, 3, 7, 9
-  
+
+  # print(list(itertools.permutations([1, 2, 3])))
+  answers = list()
   for num in range(1, 100):
-    is_circular_prime(num)
+    result = is_circular_prime(num)
+    if result:
+      answers = answers + result
+  
+  print(list(set(answers)))
+
