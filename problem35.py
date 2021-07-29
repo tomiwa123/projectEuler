7f5bea793c237c8fa9019b9c90b19e41cde67a6c
tomiwa123 <ayotomiwa.akinyele@gmail.com>
2021-07-29 12:13:39 -0700

    Problem 35

--- problem35.py
+++ problem35.py
@@ -7,3 +7,14 @@ if __name__ == '__main__':
                 return False
         return True
     
+  def is_circular_prime(num):
+    # circular prime cannot contain digits 2, 4, 6, 8, 5, 0
+    for test_num in ['0', '2', '4', '5', '6', '8']:
+      if test_num in str(num):
+        return False
+    print(num)
+    
+    # number contains only 1, 3, 7, 9
+  
+  for num in range(1, 100):
+    is_circular_prime(num)
