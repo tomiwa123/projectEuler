b508eef9a4833ba4b02eae213cb56729a7d32746
tomiwa123 <ayotomiwa.akinyele@gmail.com>
2021-08-02 17:29:46 -0700

    Problem 36

--- problem36.py
+++ problem36.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     return check_pallindrome(digits)
 
   answer = 0
-  for num in range(11, 1000000):
+  for num in range(1, 1000000):
     if check_pallindrome(list(str(num))) and check_bin_pallindrome(bin(num)):
       answer += num
 
