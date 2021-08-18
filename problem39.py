c04f2933d2966279ede305b131a142965e746ddf
tomiwa123 <ayotomiwa.akinyele@gmail.com>
2021-08-17 22:28:15 -0400

    problem 39

---
+++ problem39.py
@@ -0,0 +1,24 @@
+import math
+from collections import Counter
+
+if __name__ == '__main__':
+  
+  perimeters = dict()
+
+  for a in range(1, 1000):
+    for b in range(a, 1000):
+      c = math.sqrt(pow(a, 2) + pow(b, 2))
+      
+      if int(c) != c:
+        continue
+      
+      p = a + b + int(c)
+      
+      if not perimeters.get(p):
+        perimeters[p] = 1
+      else:
+        perimeters[p] += 1
+  
+  counter = Counter(perimeters)
+  print(perimeters)
+  print(counter.most_common()[0])
