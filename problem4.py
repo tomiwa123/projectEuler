0a7cb21cc022bf2b602ee79d5e065e93dcd5f735
tomiwa123 <ayotomiwa.akinyele@gmail.com>
2021-09-10 12:29:41 -0400

    problem 4 fix

--- problem4.py
+++ problem4.py
@@ -12,13 +12,14 @@ if __name__ == '__main__':
     for num in range(998001, 1, -1):
         if found_answer:
             break
-        if not (palindrome(num)):
+        if not palindrome(num):
             continue
-        for factor in range(100, 999):
-            if num % factor == 0 and len(str((num / factor))) == 5:
+        for factor in range(100, min(999, int(num ** 0.5))):
+            if num % factor == 0 and len(str(num / factor)) == 3:
                 found_answer = True
                 print(num)
                 factors[0] = factor
                 factors[1] = num / factor
+                break
 
     print(factors)
---
+++ problem45.py
@@ -0,0 +1,61 @@
+import cmath
+
+if __name__ == "__main__":
+  
+  def solve_quadratic(a, b, c):  
+    # calculating  the discriminant
+    dis = (b**2) - (4 * a*c)
+      
+    # find two results
+    ans1 = (-b - cmath.sqrt(dis))/(2 * a)
+    ans2 = (-b + cmath.sqrt(dis))/(2 * a)
+
+    return (ans1, ans2)
+  
+  def solve_quadratic_tri(c, a=1, b=1):
+    return solve_quadratic(a, b, -c)
+  
+  def solve_quadratic_pent(c, a=3, b=-1):
+    return solve_quadratic(a, b, -c)
+
+  def solve_quadratic_hex(c, a=4, b=-2):
+    return solve_quadratic(a, b, -c)
+  
+  def test_roots(root_1, root_2):
+    # Account for the possibility that both roots are valid
+    if root_1.imag == 0:
+      if root_1.real.is_integer() and root_1.real > 0:
+        return int(root_1.real)
+    if root_2.imag == 0:
+      if root_2.real.is_integer() and root_2.real > 0:
+        return int(root_2.real)
+    return False
+
+  for num in range(40000, 10000000):
+    # Check if num is triangle
+    solution = [num]
+    tri_sol = solve_quadratic_tri(num * 2)
+    
+    if not test_roots(*tri_sol):
+      continue
+    # print(test_roots(*tri_sol))
+    solution.append(test_roots(*tri_sol))
+
+    pent_sol = solve_quadratic_pent(num * 2)
+    
+    if not test_roots(*pent_sol):
+      continue
+    # print(test_roots(*pent_sol))
+    solution.append(test_roots(*pent_sol))
+    
+    hex_sol = solve_quadratic_hex(num * 2)
+    
+    if not test_roots(*hex_sol):
+      continue
+    # print(test_roots(*hex_sol))
+    solution.append(test_roots(*hex_sol))
+
+    print(solution)
+
+  # print(test_roots(*solve_quadratic_hex(2 * 40755)))
+
