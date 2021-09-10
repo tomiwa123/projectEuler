cf382c693570083d74bf1859d8f5176943bedbf0
tomiwa123 <ayotomiwa.akinyele@gmail.com>
2021-09-10 12:35:59 -0400

    problem 6 rename variables

--- problem6.py
+++ problem6.py
@@ -1,7 +1,7 @@
 if __name__ == '__main__':
     num_sum = 0
-    tally = 0
+    sum_of_squares = 0
     for num in range(1, 101):
         num_sum += num
-        tally += num**2
-    print(num_sum ** 2 - tally)
+        sum_of_squares += num**2
+    print(num_sum ** 2 - sum_of_squares)
