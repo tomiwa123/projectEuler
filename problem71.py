fa24b6efaf6eb66529ce80036c2e9db3257ee5a2
tomiwa123 <ayotomiwa.akinyele@gmail.com>
2021-09-12 19:46:48 -0400

    Sep 12 Project Euler Marathon Concluded

--- problem71.py
+++ problem71.py
@@ -1,20 +1,18 @@
-from math import floor, ceil
+from math import floor
 
-if __name__ == "__main__":
+smallest_diff = 1000
+best_d = 0
+best_n = 1000
+three_seven = 3.0/7
 
-  smallest_diff = 1000
-  best_d = 0
-  best_n = 1000
-  three_seven = 3.0/7
-
-  for d in range(25, 1000 * 1000):
-    floor_value = floor(d / 7) * 3
-    # ceil_value = ceil(d / 7)
-    while (floor_value / d) < three_seven:
-      test_value = floor_value / d
-      if (3/7 - test_value) < smallest_diff:
-        smallest_diff = 3/7 - test_value
-        best_d = d
-        best_n = floor_value
-      floor_value += 1
-  print(best_d, best_n, smallest_diff)
+for d in range(25, 1000 * 1000):
+  floor_value = floor(d / 7) * 3
+  # ceil_value = ceil(d / 7)
+  while (floor_value / d) < three_seven:
+    test_value = floor_value / d
+    if (3/7 - test_value) < smallest_diff:
+      smallest_diff = 3/7 - test_value
+      best_d = d
+      best_n = floor_value
+    floor_value += 1
+print(best_d, best_n, smallest_diff)
