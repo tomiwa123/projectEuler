4bd8b1d3ab941d87a6fb46b989337ba18fe5cfc1
tomiwa123 <ayotomiwa.akinyele@gmail.com>
2021-09-14 11:07:34 -0400

    problem 92

--- problem92.py
+++ problem92.py
@@ -1,28 +1,31 @@
-ones = set()
-ones.add(1)
-eighty_nines = set()
-eighty_nines.add(89)
-
 def transform(num):
   sum = 0
   for let in str(num):
     sum += int(let) ** 2
   return sum
 
+values = [0]*10000001
 for num in range(1, 10000000):
-  temp = num
-  track = {num}
+  values[num] = num
+for num in range(1, 10000000):
+  # print(num)
+  temp = values[num]
+  if temp in {1, 89}:
+    continue
+  track = {temp}
   while (temp != 1) and (temp != 89):
-  # while (temp != 1) and (temp != 89) and (temp not in ones) and (temp not in eighty_nines):
-    # print(temp)
-    temp = transform(temp)
+    new_index = transform(temp)
+    temp = values[new_index]
     track.add(temp)
-  # print(track)
-  # print(temp)
-  if temp == 1 or temp in ones:
-    ones.union(track)
-  elif temp == 89 or temp in eighty_nines:
-    eighty_nines = eighty_nines.union(track)
   
-# print(eighty_nines)
-print(len(eighty_nines))
+  update = 89
+  if temp == 1:
+    update = 1
+  for index in track:
+    values[index] = update
+
+count = 0
+for num in range(1, 10000000):
+  if values[num] == 89:
+    count += 1
+print(count)
