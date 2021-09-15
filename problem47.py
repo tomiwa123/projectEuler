30e1650486d8cbb5f5b3fbecf98b6417af858c1b
tomiwa123 <ayotomiwa.akinyele@gmail.com>
2021-09-15 13:11:42 -0400

    problem 47 attempt

--- problem47.py
+++ problem47.py
@@ -12,45 +12,73 @@ if __name__ == '__main__':
           return False
     return True
 
-  current_factors = dict()
+  # current_factors = dict()
+  current_factors = list()
 
-  def update_dict(factors):
-    current_factors[0] = current_factors[1]
-    current_factors[1] = current_factors[2]
-    current_factors[2] = current_factors[3]
-    current_factors[3] = factors
+  # def update_dict(factors):
+  #   current_factors[0] = current_factors[1]
+  #   current_factors[1] = current_factors[2]
+  #   current_factors[2] = current_factors[3]
+  #   current_factors[3] = factors
     # print(current_factors)
   
-  def check_dict(x):
-    first = current_factors[0]
-    second = current_factors[1]
-    third = current_factors[2]
-    fourth = current_factors[3]
+  # def check_dict(x):
+  #   first = current_factors[0]
+  #   second = current_factors[1]
+  #   third = current_factors[2]
+  #   fourth = current_factors[3]
     
-    return len(set(first)) == x and len(set(second)) == x and len(set(third)) == x and len(set(fourth)) == x
-    # return len(set(second)) == x and len(set(third)) == x and len(set(fourth)) == x
+  #   return len(set(first)) == x and len(set(second)) == x and len(set(third)) == x and len(set(fourth)) == x
+  #   # return len(set(second)) == x and len(set(third)) == x and len(set(fourth)) == x
   
-  def get_prime_factors(num):
-    number = num
-    factors = []
+  def check_list(x):
+    length = len(current_factors) - 1
+    for num in range(length, length - x):
+      if current_factors[num] != x:
+        return False
+    return True
 
-    while number > 1:
-      for divisor in range(2, number + 1):
-        if is_prime(divisor) and number % divisor == 0:
-          number /= divisor
-          factors.append(divisor)
-          break
-    # print(num, factors)
-    return factors
+  # def get_prime_factors(num):
+  #   number = num
+  #   factors = []
 
-  current_factors[0] = get_prime_factors(210)
-  current_factors[1] = get_prime_factors(211)
-  current_factors[2] = get_prime_factors(212)
-  current_factors[3] = get_prime_factors(213)
+  #   while number > 1:
+  #     for divisor in range(2, number + 1):
+  #       if is_prime(divisor) and number % divisor == 0:
+  #         number /= divisor
+  #         factors.append(divisor)
+  #         break
+  #   # print(num, factors)
+  #   return factors
 
-  for num in range(1000,150000):
-    update_dict(get_prime_factors(num))
-    if check_dict(4):
-      print(num-3)
+  def count_prime_factors(num):
+    count = 0
+    for divisor in range(2, num + 1):
+      if is_prime(divisor) and num % divisor == 0:
+        if num == divisor:
+          current_factors.append(1)
+          return 1
+        factor_count = current_factors[num / divisor]
+        if (num / divisor) % divisor == 0:
+          current_factors.append(factor_count)
+          count = factor_count
+        else:
+          current_factors.append(factor_count + 1)
+          count = factor_count + 1
+        break
+    return count
+
+  # current_factors[0] = get_prime_factors(210)
+  # current_factors[1] = get_prime_factors(211)
+  # current_factors[2] = get_prime_factors(212)
+  # current_factors[3] = get_prime_factors(213)
+  current_factors.append(0)
+  current_factors.append(1)
+
+  for num in range(2, 1000):
+    count_prime_factors(num)
+    if check_list(3):
+      # print(current_factors)
+      print(num-2)
       break
 
